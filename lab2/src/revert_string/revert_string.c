bd2a72520d8f3c5d320c5cd77dd0dc685d9c131c
bernitbr <bernitbr@student.21-school.ru>
2026-09-24 19:31:38 +0300

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
-}
+	int left = 0;
+	int right = (int)strlen(str) - 1;
 
+	while (left < right)
+	{
+		char tmp = str[left];
+		str[left] = str[right];
+		str[right] = tmp;
+		left++;
+		right--;
+	}
+}
